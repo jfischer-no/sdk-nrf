059ff2d7b652a25c4c3b3896684020bdbd977ba8
Johann Fischer <johann.fischer@nordicsemi.no>
2021-01-21 23:08:37 +0100

    zigbee: zb_nrf_crypto: obtain instance from devicetree
    Adapt to upstream changes in the commit d855d472889c
    ("drivers: crypto_nrf_ecb: Convert to use devicetree")
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- subsys/zigbee/osif/zb_nrf_crypto.c
+++ subsys/zigbee/osif/zb_nrf_crypto.c
@@ -105,7 +105,7 @@ zb_uint32_t zb_random_seed(void)
 void zb_osif_aes_init(void)
 {
 #if CONFIG_CRYPTO_NRF_ECB
-	dev = device_get_binding(CONFIG_CRYPTO_NRF_ECB_DRV_NAME);
+	dev = device_get_binding(DT_LABEL(DT_INST(0, nordic_nrf_ecb)));
 	__ASSERT(dev, "Crypto driver not found");
 #endif
 }
